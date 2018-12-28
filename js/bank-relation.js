b0dfcaf740724708a1d29976d7ddafc760de403b
notek <notek1314@gmail.com>
2018-12-28 19:02:14 +0800

    u

--- js/bank-relation.js
+++ js/bank-relation.js
@@ -52,4 +52,301 @@ $(() => {
         }
         console.log(startDate, endDate);
     });
+
+    let timer = null;
+    function drawTreeChart(links, linksLeft) {
+        let width = $('.chart').width(),
+            height = $('.chart').height();
+
+        let zoom_handler = d3.zoom().on('zoom', zoom_actions);
+        let canvas = d3.select('svg')
+            .style('width', width)
+            .style('height', height)
+            .call(zoom_handler);
+            
+        let svg = canvas.append('g')
+            .attr('class', 'g-box');
+
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            this.isAllowRequest = true;
+            let bbox = $('.g-box')[0].getBBox();
+            let sWid = $('svg').width();
+            let sHei = $('svg').height();
+
+            $('.g-box').attr('transform', 'translate(' + (Math.abs(bbox.x) + (sWid - bbox.width) / 2) + ',' + (Math.abs(bbox.y) + (sHei - bbox.height) / 2) + ')');
+            // 设置拖拽和缩放的原点位置.
+            canvas.call(zoom_handler.transform, d3.zoomIdentity.translate((Math.abs(bbox.x) + (sWid - bbox.width) / 2), (Math.abs(bbox.y) + (sHei - bbox.height) / 2)).scale(1));
+        }, 100);
+
+        let defs = svg.append('svg:defs')
+        let arrow = defs.append('svg:marker')    // This section adds in the arrows
+            .attr('id', 'sell')
+            .attr('refX', 9)
+            .attr('refY', 2)
+            .attr('markerWidth', 10)
+            .attr('markerHeight', 10)
+            .attr('orient', 'auto')
+            .append('svg:path')
+            .attr('d', 'M0,0 L0,4 L9,2 z')
+            .attr('fill', '#999');
+        // right arrow.
+        defs.append('svg:marker')    // This section adds in the arrows
+            .attr('id', 'buy')
+            .attr('refX', 0)
+            .attr('refY', 2)
+            .attr('markerWidth', 10)
+            .attr('markerHeight', 10)
+            .attr('orient', '0')
+            .append('svg:path')
+            .attr('d', 'M0,0 L0,4 L9,2 z')
+            .attr('fill', '#999');
+
+
+        let stratify = d3.stratify()
+            .parentId(function(d) { return d.pid; });
+
+        let links_data = links;
+        let links_data_left = linksLeft;
+
+        // 节点数量过多导致重叠 设置基础节点数200 超过则取阀值进行扩展.
+        let maxCnt = Math.max(links_data.length, links_data_left.length);
+        let basicCnt = 110, threshold = 0;
+        if(maxCnt > basicCnt) {
+            threshold = (basicCnt - maxCnt) * (maxCnt / basicCnt) * 5;
+        }else {
+            threshold = (basicCnt - maxCnt) * 12;
+        }
+
+        let tree = d3.tree().size([height - threshold, width / 2 - 160]);
+
+        let root = stratify(links_data)
+            .sort(function(a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); });
+
+        let root_left = stratify(links_data_left)
+            .sort(function(a, b) { return (a.height - b.height) || a.id.localeCompare(b.id); });
+
+        tree(root);
+        tree(root_left)
+        flip_nodes(root_left, root);
+        let exLinks = buildExtraLinks(root);
+        exLinks = exLinks.concat(buildExtraLinks(root_left));
+        
+        if(root.children) {
+            root.children = root.children.concat(root_left.children);
+            drawTree(root, 'left');
+        }else {
+            root.children = [];
+            drawTree(root_left, 'left');
+        }
+
+        //Zoom functions 
+        function zoom_actions() {
+            svg.attr('transform', d3.event.transform)
+        }
+
+        function flip_nodes(root, root_ref) {
+            let yy = 2 * root.y,
+                xx = root_ref.x + root.x;
+            root.descendants().forEach(function(d) {
+                d.y = yy - d.y;
+                d.x = xx - d.x;
+            })
+        }
+
+        function buildExtraLinks(root) {
+            let links = root.descendants();
+            let nodeMap = {};
+            let outputLinks = []
+            links.forEach(function(d) {
+                nodeMap[d.id] = d;
+            })
+
+            return outputLinks;
+        }
+
+        function drawTree(root, ori) {
+            let link = svg.append('g')
+                .selectAll('.tree-link')
+                .data(root.descendants().slice(1))
+                .enter().append('g');
+
+            let path = link.append('path')
+                .attr('class', d => {
+                    return `tree-link ${d.parent.data.id.replace(/#/g, '-')}`;
+                })
+                .attr('id', d => {
+                    return d.data.name
+                })
+                .attr('d', diagonal)
+                .attr('marker-end', d => {
+                    if (d.data.activity == 'sell') {
+                        return `url(#${d.data.activity})`
+                    }
+                })
+                .attr('marker-start', d => {
+                    if(d.data.activity == 'buy') {
+                        return `url(#${d.data.activity})`
+                    }
+                })
+                .attr('stroke', '#337ab7');
+
+            // let textpath = link.append('text')
+            //     .attr('fill', function(d) {
+            //         if(d.data.activity === 'buy') {
+            //             return '#d9534f';
+            //         }
+            //         return '#5cb85c';
+            //     })
+            //     .append('textPath')
+            //     .attr('href', function(d) { 
+            //         return '#' + d.data.name;
+            //     })
+            //     .attr('startOffset', '30%')
+            //     .style('text-anchor', 'middle')
+            //     .text(function(d) { return d.data.value.toFixed(2); });
+
+            let node = svg.selectAll('.node')
+                .data(root.descendants())
+                .enter().append('g')
+                .attr('class', function(d) { return 'node' + (d.children ? ' node--internal' : ' node--leaf'); })
+                .attr('transform', function(d) { return 'translate(' + d.y + ',' + d.x + ')'; });
+
+            let nametext = node.append('text')
+                .style('text-anchor', function(d) {
+                    if(d.data.lv === 0) {
+                        return 'start';
+                    }
+
+                    if (d.data.activity == 'buy')
+                        return d.children ? 'end' : 'start';
+                    else
+                        return d.children ? 'start' : 'end';
+                })
+                .style('font-size', function(d) {
+                    if(d.data.lv === 0) {
+                        return '14';
+                    }
+                })
+                .attr('fill', function(d) {
+                    if(d.data.lv === 0) {
+                        return '#000';
+                    }else {
+                        return '#666';
+                    }
+                })
+                .attr('stroke', function(d) {
+                    if(d.data.lv === 0) {
+                        return '#000';
+                    }
+                });
+
+            // 处理文本的x距离.
+            function processTextOfX(d) {
+                if(d.data.lv === 0) {
+                    return -40;
+                }
+
+                if(d.data.activity === 'buy') {
+                    return 18;
+                }else {
+                    return -5;
+                }
+            };
+            nametext.append('tspan')
+                .attr('x', processTextOfX)
+                .attr('y', function(d) {
+                    if(d.data.lv === 0) {
+                        return 15;
+                    }
+
+                    return -10;
+                })
+                .text(function(d) { return d.data.name; });
+
+            nametext.append('tspan')
+                .attr('x', processTextOfX)
+                .attr('y', function(d) {
+                    if(d.data.lv === 0) {
+                        return 32;
+                    }
+
+                    return 5;
+                })
+                .text(function(d) { return d.data.account_number; });
+
+            nametext.append('tspan')
+                .attr('x', processTextOfX)
+                .attr('y', function(d) {
+                    if(d.data.lv === 0) {
+                        return 49;
+                    }
+
+                    return 19;
+                })
+                .text(function(d) { return d.data.related_bank; });
+
+            // define background color for link labels
+            let txtBgnd = defs.append('filter')
+                .attr('id', 'solid')
+                .attr('x', 0)
+                .attr('y', 0)
+                .attr('width', 1)
+                .attr('height', 1)
+                .style('opacity', 0.5);
+            txtBgnd.append('feFlood')
+                .attr('flood-color', 'rgb(255, 255, 255)');
+            txtBgnd.append('feComposite')
+                .attr('in', 'SourceGraphic');
+            node.append('text')
+                .attr('filter', 'url(#solid)')
+                .style('display', function(d) {
+                    if(d.data.lv === 0) {
+                        return 'none';
+                    }
+                })
+                .style('stroke', function(d) {
+                    return '#999';
+                })
+                .attr('x', function(d) {
+                    if(d.data.activity === 'buy') {
+                        return -140;
+                    }
+                    return 100;
+                })
+                .attr('dy', function(d) {
+                    return 0;
+                })
+                .text(function(d) { return d.data.value.toFixed(2);});
+
+            function diagonal(d) {
+                if (d.data.activity == 'buy') {
+                    return 'M' + d.y + ',' + d.x
+                        + 'C' + (d.parent.y + 100) + ',' + d.x
+                        + ' ' + (d.parent.y + 100) + ',' + d.parent.x
+                        + ' ' + d.parent.y + ',' + d.parent.x
+                        + 'T' + d.parent.y + ',' + d.parent.x;
+                }else {
+                    return 'M' + d.y + ',' + d.x
+                        + 'C' + (d.parent.y - 100) + ',' + d.x
+                        + ' ' + (d.parent.y - 100) + ',' + d.parent.x
+                        + ' ' + (d.parent.y - 20) + ',' + d.parent.x
+                        + 'T' + d.parent.y + ',' + d.parent.x;
+                }
+            }
+        }
+
+    };
+
+    $.ajax({
+        type: 'GET',
+        data: {},
+        url: '../js/mock/relations.json',
+        success: data => {
+            if(data.status_code === 0) {
+                console.log(data)
+                drawTreeChart(data.data.in, data.data.out);
+            }
+        }
+    });
 });
