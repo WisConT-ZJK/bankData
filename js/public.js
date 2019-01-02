675a7554bff62b13a44c98b5216f502fcabc03ef
zhangjianakng <jk_z@outlook.com>
2019-01-02 16:01:44 +0800

    search problem

--- js/public.js
+++ js/public.js
@@ -15,3 +15,14 @@ function checkLoginStatus() {
 
     return true;
 };
+
+$.extend({ alert: function (message, title) {
+    $("<div></div>").dialog( {
+      buttons: { "Ok": function () { $(this).dialog("close"); } },
+      close: function (event, ui) { $(this).remove(); },
+      resizable: false,
+      title: title,
+      modal: true
+    }).text(message);
+  }
+  });
