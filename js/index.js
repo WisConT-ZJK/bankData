a1b58375e6f0e052aa5477044ae119d25b327bb0
notek <notek1314@gmail.com>
2019-01-09 17:22:05 +0800

    搜索功能

--- js/index.js
+++ js/index.js
@@ -13,35 +13,50 @@ $(() => {
             return;
         }
 
+        $('.search-result .ex-tips').show();
+        $('.search-result .ex-tips').html('搜索中请稍等...');
+        $('button.search-btn').addClass('disabled');
+        $('.search-result >table, .res-tips').hide();
+        $('.search-result >table tbody').empty();
+
         $.ajax({
             type: 'GET',
-            data: {},
-            url: '../js/mock/search.json',
+            data: {
+                keyword: queryStr
+            },
+            url: '/api/bank_data/account_search/',
+            // url: '../js/mock/search.json',
             success: data => {
-                if(data.status_code === 0) {
-                    let htmlstr = ``;
-                    data.data.forEach(function(d) {
-                        htmlstr += `
-                            <tr data-id="${d.id}" class="n-table--hover">
-                                <td>${d.nsrsbh}</td>
-                                <td>${d.companyName}</td>
-                                <td>${d.bankAccountName}</td>
-                                <td>${d.bankAccount}</td>
-                                <td>${d.ofBank}</td>
-                            </tr>
-                        `;
-                    });
+                if(data.status === 0) {
+                    $('#bank-search').val('').focus();
+                    $('button.search-btn').removeClass('disabled');
 
-                    $('.search-result >p').hide();
-                    $('.search-result >table').show();
+                    if(data.data.length > 0) {
+                        let htmlstr = ``;
+                        data.data.forEach(function(d) {
+                            htmlstr += `
+                                <tr data-id="${d.id}" class="n-table--hover">
+                                    <td>${d.identity_number || '暂无'}</td>
+                                    <td>${d.account_name || '暂无'}</td>
+                                    <td>${d.account_number || '暂无'}</td>
+                                    <td>${d.bank_related || '暂无'}</td>
+                                </tr>
+                            `;
+                        });
 
-                    $('.search-result >table tbody').empty();
-                    $('.search-result >table tbody').append(htmlstr);
-                    $('.search-result >table tbody tr').on('click', function() {
-                        //console.log($(this).attr('data-id'));
-                        // todo 跳转到数据详情页
-                        window.location.href = '../data-find.html?data-id='+$(this).attr('data-id')
-                    });
+                        $('.search-result .ex-tips').hide();
+                        $('.search-result >table, .res-tips').show();
+                        $('.res-tips').html(`搜索<em style="font-style:normal;color:#49a8f4">「${queryStr}」</em>关键字结果如下：`);
+
+                        $('.search-result >table tbody').empty();
+                        $('.search-result >table tbody').append(htmlstr);
+                        $('.search-result >table tbody tr').on('click', function() {
+                            window.location.href = '../data-find.html?data-id='+$(this).attr('data-id')
+                        });
+                    }else {
+                        $('.search-result .ex-tips').show();
+                        $('.ex-tips').html('搜索数据为空，请重新搜索');
+                    }
                 }
             }
         });
